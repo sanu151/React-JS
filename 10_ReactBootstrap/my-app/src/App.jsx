bbb265d0eb5bf37da22ed0121c32db30825c4485
Supriyo Das <das.supriyo07@gmail.com>
2024-10-21 05:41:05 +0530

    React with Bootstrap

---
+++ 10_ReactBootstrap/my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card1 from "./components/Card1";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>React With Bootstrap</h1>
+        <Card1 className="card" />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ 10_ReactBootstrap/my-app/src/components/Card1.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+import { Card, Button } from "react-bootstrap";
+
+const Card1 = () => {
+  return (
+    <div>
+      <Card>
+        <h1>Card Title</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eveniet
+          consectetur incidunt odio sit perspiciatis?
+        </p>
+        <Button>Learn More</Button>
+      </Card>
+    </div>
+  );
+};
+
+export default Card1;
