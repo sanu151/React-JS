457f151736264ff1aba0dc379138d07e9440a15f
Supriyo Das <das.supriyo07@gmail.com>
2024-10-22 06:11:31 +0530

    Nested Mapping

---
+++ 07_NestedMapping/my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import List from "./components/List";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Nested Mapping</h1>
+        <List />
+      </div>
+    </>
+  );
+}
+
+export default App;
