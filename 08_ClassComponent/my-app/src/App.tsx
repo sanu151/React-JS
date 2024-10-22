d3ef409209055aed8c32fb417ad99423bef08c6d
Supriyo Das <das.supriyo07@gmail.com>
2024-10-22 06:12:20 +0530

    Class Component

---
+++ 08_ClassComponent/my-app/src/App.tsx
@@ -0,0 +1,16 @@
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Card
+          cardTitle="Card Title"
+          cardDesc="This is Card Description for Practice of Create React Class Component"
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
