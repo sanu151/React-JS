a692e6bc97f575fe611a5c35d581d70f2566c542
Supriyo Das <das.supriyo07@gmail.com>
2024-10-25 06:48:18 +0530

    Controlled Component | Collect Form Data

---
+++ 19_ControlledComponent/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Form from "./components/Form/Form";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Controlled component | Collect form data</h1>
+        <Form />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ 19_ControlledComponent/my-app/src/components/Form/Form.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import style from "./form.module.css";
+
+const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userInfo = {
+      name,
+      email,
+      password,
+    };
+    console.log(userInfo);
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>User Registration</h2>
+      <form action="" onSubmit={handleSubmit}>
+        <div className={style.formGroup}>
+          <label htmlFor="name">Name : </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={handleNameChange}
+            required
+          />
+        </div>
+        <div className={style.formGroup}>
+          <label htmlFor="email">Email : </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div className={style.formGroup}>
+          <label htmlFor="password">Password : </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <div className={style.formGroup}>
+          <button type="submit">Register</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
